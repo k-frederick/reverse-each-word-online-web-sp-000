9dfbaaa870ffec293b29c1e7a68760d61b7c7384
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-28 22:50:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 def reverse_each_word(sentence1)
   sentence1_split = sentence1.split(" ")
-
-
-
-  #sentence1.each do |word|
-    #puts word.reverse
+  sentence1_split.each do |word|
+    word.reverse
 
   #end
 end
